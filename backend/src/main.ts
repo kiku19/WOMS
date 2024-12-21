f8ed512c91b0da018b962c193ea83dd6f23435c6
kishore <kbalu@edvak.com>
2024-12-21 23:31:29 +0530

    allowed every origin

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -33,9 +33,9 @@ console.log("connected to db");
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://womsfrontend.netlify.app/",
+    origin: "*",
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
+    // credentials: true,
   })
 );
 
